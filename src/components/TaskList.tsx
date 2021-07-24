9fb48768f8a855764de70091f1bfb04c651abbe0
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-07-24 13:10:06 -0300

    chapter1 finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(newTaskTitle !== '') {
+      const newTask = {
+        id: Math.round((Math.random() * 20) + 1),
+        title: newTaskTitle,
+        isComplete: false
+      }
+      setTasks([...tasks, newTask])
+    } else {
+      alert('não é possível adicionar tarefas vazias!')
+    }
+    
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map((task) => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete
+      }
+      return task;
+    }))
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task) => task.id !== id))
   }
 
   return (
